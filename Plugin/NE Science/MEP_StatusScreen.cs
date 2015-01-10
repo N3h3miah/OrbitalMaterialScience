d8c4a829edfbbb73618546621673293bb88e93b4
Emanuel <emanuel@graf.name>
2015-01-10 22:35:21 +0100

    MEP IVA with prop

---
+++ Plugin/NE Science/MEP_StatusScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace NE_Science
+{
+    class MEP_StatusScreen : InternalModule
+    {
+        private double lastUpdate = 0;
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (lastUpdate + 2 < Time.time)
+            {
+                lastUpdate = Time.time;
+                ExposureLab lab = part.GetComponent<ExposureLab>();
+                NE_Helper.log("MEP Lab Status: " + lab.labStatus);
+
+            }
+        }
+    }
+}
