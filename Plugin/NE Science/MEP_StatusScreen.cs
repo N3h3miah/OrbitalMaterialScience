55e038fd344709291dee9148cb1a27ff79b0b218
Emanuel <emanuel@graf.name>
2015-01-11 12:51:45 +0100

    MEP IVA Status Screen

--- Plugin/NE Science/MEP_StatusScreen.cs
+++ Plugin/NE Science/MEP_StatusScreen.cs
@@ -10,16 +10,129 @@ namespace NE_Science
     {
         private double lastUpdate = 0;
 
+        [KSPField(isPersistant = false)]
+        public string folder = "NehemiahInc/Props/MEP_StatusScreen";
+
+        [KSPField(isPersistant = false)]
+        public string notReadyTexture = "";
+
+        [KSPField(isPersistant = false)]
+        public string readyTexture = "";
+
+        [KSPField(isPersistant = false)]
+        public string runningTexture = "";
+
+        [KSPField(isPersistant = false)]
+        public string errorTexture = "";
+
+        [KSPField(isPersistant = false)]
+        public float refreshInterval = 2;
+
+        private GameDatabase.TextureInfo notReady;
+        private GameDatabase.TextureInfo ready;
+        private GameDatabase.TextureInfo running;
+        private GameDatabase.TextureInfo error;
+        private Material screenMat = null;
+
+        private int lastLabStatus = -1;
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+        }
+
         public override void OnUpdate()
         {
             base.OnUpdate();
-            if (lastUpdate + 2 < Time.time)
+            if (lastUpdate + refreshInterval < Time.time)
             {
                 lastUpdate = Time.time;
                 ExposureLab lab = part.GetComponent<ExposureLab>();
-                NE_Helper.log("MEP Lab Status: " + lab.labStatus);
+
+                if (lab.MEPlabState != lastLabStatus)
+                {
+                    GameDatabase.TextureInfo newTexture = getTextureForState(lab.MEPlabState);
+                    if (newTexture != null)
+                    {
+                        changeTexture(newTexture);
+                        
+                    }
+                    else
+                    {
+                        NE_Helper.logError("New Texture null");
+                    }
+                    lastLabStatus = lab.MEPlabState;
+                }
 
             }
+        }
+
+        private void changeTexture(GameDatabase.TextureInfo newTexture)
+        {
+            Material mat = getScreenMaterial();
+            if (mat != null)
+            {
+                NE_Helper.log("Old Texture: " + mat.mainTexture.name);
+                NE_Helper.log("Set new Texture: " + newTexture.name);
+                mat.mainTexture = newTexture.texture;
+            }
+            else
+            {
+                NE_Helper.logError("Transform NOT found: " + "MEP IVA Screen");
+            }
+        }
+
+        private Material getScreenMaterial()
+        {
+            if (screenMat == null)
+            {
+                Transform t = internalProp.FindModelTransform("MEP IVA Screen");
+                if (t != null)
+                {
+                    NE_Helper.log("Transform found: " + "MEP IVA Screen");
+                    screenMat = t.renderer.material;
+                    return screenMat;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return screenMat;
+            }
+        }
+
+        private GameDatabase.TextureInfo getTextureForState(int p)
+        {
+            switch (p)
+            {
+                case NE_Helper.MEP_NOT_READY:
+                    if (notReady == null) notReady = getTexture(folder, notReadyTexture);
+                    return notReady;
+
+                case NE_Helper.MEP_READY:
+                    if (ready == null) ready = getTexture(folder, readyTexture);
+                    return ready;
+
+                case NE_Helper.MEP_RUNNING:
+                    if (running == null) running = getTexture(folder, runningTexture);
+                    return running;
+
+                case NE_Helper.MEP_ERROR_ON_START:
+                case NE_Helper.MEP_ERROR_ON_STOP:
+                    if (error == null) error = getTexture(folder, errorTexture);
+                    return error;
+
+                default:
+                    return null;
+            }
+        }
+
+        private GameDatabase.TextureInfo getTexture(string folder, string textureName)
+        {
+            return GameDatabase.Instance.GetTextureInfoIn(folder, textureName);
         }
     }
 }
