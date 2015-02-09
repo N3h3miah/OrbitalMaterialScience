f5747f46a4dbdd23d2c43586f2bee8fe92fc6931
Michael WERLE <micha@michaelwerle.com>
2015-02-09 14:34:56 +0000

    Improve run.sh - ensure log files are in KSP game directory

--- run.sh
+++ run.sh
@@ -9,14 +9,15 @@ mkdir -p "${DST_DIR}/Plugins"
 cp -ax "${SRC_DIR}/GameData/NehemiahInc/." "${DST_DIR}"
 cp -ax "${SRC_DIR}/Plugin/NE Science/bin/Debug/NE_Science.dll" "${DST_DIR}/Plugins/"
 
+cd "${KSP_DIR}"
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
 	echo "TODO: Add command to start KSP under OS X"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under Linux platform
-	"${KSP_DIR}/KSP.x86_64"
+	"./KSP.x86_64"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under Windows NT platform
-	"${KSP_DIR}/KSP.exe"
+	"./KSP.exe"
 fi
-
+cd "${SRC_DIR}"
