60f7c3b12dd5d22aa86dcc2ec95566665a0e51a3
Michael Werle <micha@michaelwerle.com>
2015-03-12 01:11:31 +0000

    Additional robustness to MultiStepExperiments: - mark some classes as abstract to ensure they can never be accidentally instantiated - ensure MultiStepExperiment base class allocates the steps[] array (assert: steps[] is never null) - catch NullReferenceException in getNode()

--- Plugin/NE Science/MEP_ExperimentData.cs
+++ Plugin/NE Science/MEP_ExperimentData.cs
@@ -5,15 +5,15 @@ using System.Text;
 
 namespace NE_Science
 {
-    public class MEPExperimentData : MultiStepExperimentData<MEPResourceExperimentStep>
+    public abstract class MEPExperimentData : MultiStepExperimentData<MEPResourceExperimentStep>
     {
 
         private Guid cachedVesselID;
         private int partCount;
         private List<MEP_Module> physicsLabCache = null;
 
-        protected MEPExperimentData(string id, string type, string name, string abb, EquipmentRacks eq, float mass)
-            : base(id, type, name, abb, eq, mass)
+        protected MEPExperimentData(string id, string type, string name, string abb, EquipmentRacks eq, float mass, int numSteps)
+            : base(id, type, name, abb, eq, mass, numSteps)
         { }
 
         public override List<Lab> getFreeLabsWithEquipment(Vessel vessel)
@@ -52,10 +52,8 @@ namespace NE_Science
     public class MEE1_ExperimentData : MEPExperimentData
     {
         public MEE1_ExperimentData(float mass)
-            : base("NE_MEE1", "MEE1", "Material Exposure Experiment 1", "MEE1", EquipmentRacks.EXPOSURE, mass)
+            : base("NE_MEE1", "MEE1", "Material Exposure Experiment 1", "MEE1", EquipmentRacks.EXPOSURE, mass, 2)
         {
-
-            steps = new MEPResourceExperimentStep[2];
             steps[0] = new MEPResourceExperimentStep(this, Resources.LAB_TIME, 1, "Preparation", 0);
             steps[1] = new MEPResourceExperimentStep(this, Resources.EXPOSURE_TIME, 20, "Exposure", 1);
 
@@ -65,9 +63,8 @@ namespace NE_Science
     public class MEE2_ExperimentData : MEPExperimentData
     {
         public MEE2_ExperimentData(float mass)
-            : base("NE_MEE2", "MEE2", "Material Exposure Experiment 2", "MEE2", EquipmentRacks.EXPOSURE, mass)
+            : base("NE_MEE2", "MEE2", "Material Exposure Experiment 2", "MEE2", EquipmentRacks.EXPOSURE, mass, 3)
         {
-            steps = new MEPResourceExperimentStep[3];
             steps[0] = new MEPResourceExperimentStep(this, Resources.LAB_TIME, 1, "Preparation", 0);
             steps[1] = new MEPResourceExperimentStep(this, Resources.EXPOSURE_TIME, 40, "Exposure", 1);
             steps[2] = new MEPResourceExperimentStep(this, Resources.LAB_TIME, 2, "Store Samples", 2);
