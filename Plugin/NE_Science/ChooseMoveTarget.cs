c94b07cb747ccaa07cfc265adfdc4b96d40bdef2
Michael Werle <micha@michaelwerle.com>
2017-05-15 17:33:31 +0100

    UI rework - update "Move Experiment" dialog
    TODO: Get mouse-over and part highlighting working.
    Has experimental (and non-working) tooltip update code for PopupDialog.

--- Plugin/NE_Science/ChooseMoveTarget.cs
+++ Plugin/NE_Science/ChooseMoveTarget.cs
@@ -1,6 +1,6 @@
 ﻿/*
  *   This file is part of Orbital Material Science.
- *   
+ *
  *   Orbital Material Science is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
@@ -16,21 +16,18 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace NE_Science 
+namespace NE_Science
 {
     class ChooseMoveTarget : MonoBehaviour
     {
-        private List<ExperimentStorage> targets = new List<ExperimentStorage>();
-        private ExperimentData exp;
+        private List<ExperimentStorage> targets = null; //new List<ExperimentStorage>();
+        private ExperimentData exp = null;
 
-        private bool showGui = false;
-
-        private Rect moveWindowRect = new Rect(Screen.width - 250, Screen.height / 2 - 250, 200, 400);
-        private Vector2 moveScrollPos = new Vector2();
-        private int windowID;
+        private string lastTooltip = "";
+        private PopupDialog pd = null;
 
         internal void showDialog(List<ExperimentStorage> targets, ExperimentData experimentData)
         {
@@ -38,67 +35,108 @@ namespace NE_Science
             this.targets = targets;
             exp = experimentData;
             NE_Helper.log("init done");
-            windowID = WindowCounter.getNextWindowID();
-            showGui = true;
+
+            showMoveWindow();
         }
 
-        void OnGUI()
+        // Even though we're using the new GUI, we still need the OnGUI event to parse tooltips
+        public void OnGUI()
         {
-            if (showGui)
-            {
-                showMoveWindow();
-                
+            if (Event.current.type == EventType.Repaint) {
+                updateHighlighting();
             }
         }
 
-        void showMoveWindow()
+        private void updateHighlighting()
         {
-            moveWindowRect = GUI.Window(windowID, moveWindowRect, showMoveGui, "Move Experiment");
-        }
-
-        void showMoveGui(int id)
-        {
-
-            GUILayout.BeginVertical();
-            GUILayout.Label("Choose Target");
-            moveScrollPos = GUILayout.BeginScrollView(moveScrollPos, GUILayout.Width(180), GUILayout.Height(320));
-
-            for( int i = 0, count = targets.Count; i < count; i++)
+            if (GUI.tooltip == lastTooltip)
             {
-                var e = targets[i];
-                if (GUILayout.Button( new GUIContent(e.identifier, i.ToString())))
+                return;
+            }
+
+            if (lastTooltip != "")
+            {
+                // Event: leave control
+            }
+            if (GUI.tooltip != "")
+            {
+                // Event: enter control
+                try
                 {
-                    exp.moveTo(e);
-                    closeGui();
+                    int hoverIndex = int.Parse(GUI.tooltip);
+                    targets[hoverIndex].part.SetHighlightColor(Color.cyan);
+                    targets[hoverIndex].part.SetHighlightType(Part.HighlightType.AlwaysOn);
+                    targets[hoverIndex].part.SetHighlight(true, false);
+                }
+                catch (FormatException)
+                {
+                    resetHighlight();
                 }
             }
-
-            GUILayout.EndScrollView();
-            if (GUILayout.Button("Close"))
-            {
-                closeGui();
-            }
-            GUILayout.EndVertical();
-
-            String hover = GUI.tooltip;
-            try
-            {
-                int hoverIndex = int.Parse(hover);
-                targets[hoverIndex].part.SetHighlightColor(Color.cyan);
-                targets[hoverIndex].part.SetHighlightType(Part.HighlightType.AlwaysOn);
-                targets[hoverIndex].part.SetHighlight(true, false);
-            }
-            catch (FormatException)
-            {
-                resetHighlight();
-            }
-            GUI.DragWindow();
+            lastTooltip = GUI.tooltip;
         }
 
-        private void closeGui()
+        private void showMoveWindow()
+        {
+            // This is a list of content items to add to the dialog
+            List<DialogGUIBase> dialog = new List<DialogGUIBase>();
+
+            dialog.Add(new DialogGUILabel("Choose destination experiment storage slot for " + exp.getName(), true, true));
+            dialog.Add(new DialogGUISpace(4));
+
+            // Build a button list of all available experiments with their descriptions
+            int numTargets = targets.Count;
+            DialogGUIBase[] scrollList = new DialogGUIBase[numTargets + 1];
+            scrollList[0] = new DialogGUIContentSizer(ContentSizeFitter.FitMode.Unconstrained, ContentSizeFitter.FitMode.PreferredSize, true);
+            for (int i = 0; i < numTargets; i++)
+            {
+                var e = targets[i];
+                var b = new DialogGUIButton<ExperimentStorage>(e.identifier, onSelectTarget, e, true);
+                b.tooltipText = i.ToString(); // TODO: Work out hover
+                b.size = new Vector2(120, 30);
+                //var l = new DialogGUILabel(e.getDescription(), true, true);
+                var h = new DialogGUIHorizontalLayout(true, false, 4, new RectOffset(), TextAnchor.UpperLeft, new DialogGUIBase[] { b });
+
+                scrollList[i + 1] = h;
+            }
+
+            dialog.Add(new DialogGUIScrollList(new Vector2(200,300), false, true, //Vector2.one, false, true,
+                new DialogGUIVerticalLayout(10, 100, 4, new RectOffset(6, 24, 10, 10), TextAnchor.UpperLeft, scrollList)
+            ));
+            dialog.Add(new DialogGUISpace(4));
+            dialog.Add(new DialogGUILabel(getLastTooltip, true, true));
+            dialog.Add(new DialogGUISpace(4));
+            // Add a centered "Cancel" button
+            dialog.Add(new DialogGUIHorizontalLayout(new DialogGUIBase[]
+            {
+                new DialogGUIFlexibleSpace(),
+                new DialogGUIButton("Close", onCloseMoveWindow, true),
+                new DialogGUIFlexibleSpace(),
+            }));
+
+            // Actually create and show the dialog
+            pd = PopupDialog.SpawnPopupDialog(
+                new MultiOptionDialog("", "Choose Target", HighLogic.UISkin, dialog.ToArray()),
+                false, HighLogic.UISkin);
+        }
+
+        private string getLastTooltip()
+        {
+            return "Tooltip: " + lastTooltip;
+        }
+
+        void onSelectTarget(ExperimentStorage es)
+        {
+            exp.moveTo(es);
+            onCloseMoveWindow();
+        }
+
+        private void onCloseMoveWindow()
         {
             resetHighlight();
-            showGui = false;
+            pd = null;
+            targets = null;
+            exp = null;
         }
 
         private void resetHighlight()
