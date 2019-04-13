f9fd8d302523bd47d2fae8c6ef0cd3c6bffba406
Michael Werle <micha@michaelwerle.com>
2019-04-14 02:34:04 +0900

    Update version numbers and compiled plugin.

--- Plugin/NE_Science/Properties/AssemblyInfo.cs
+++ Plugin/NE_Science/Properties/AssemblyInfo.cs
@@ -29,11 +29,11 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.2.33")]
+[assembly: AssemblyVersion("0.8.3.34")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("0.8.2 Debug for KSP 1.4+")]
+[assembly: AssemblyInformationalVersion("0.8.3 Debug for KSP 1.7+")]
 #else
-[assembly: AssemblyInformationalVersion("0.8.2 for KSP 1.4+")]
+[assembly: AssemblyInformationalVersion("0.8.3 for KSP 1.7+")]
 #endif
-[assembly: AssemblyFileVersion("0.8.2.33")]
-[assembly: KSPAssembly("NE_Science", 0, 82)]
+[assembly: AssemblyFileVersion("0.8.3.34")]
+[assembly: KSPAssembly("NE_Science", 0, 83)]
